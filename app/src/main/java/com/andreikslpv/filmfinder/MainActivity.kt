3aa59a47e256d1d4db5b9f8de45f86413d01fe63
Andrey <andrei.kslpv@yandex.ru>
2022-08-31 20:24:14 +0300

    3: Homework_20: изменен прототип верстки главного меню в соответствии с практическим заданием модуля 20

--- app/src/main/java/com/andreikslpv/filmfinder/MainActivity.kt
+++ app/src/main/java/com/andreikslpv/filmfinder/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.andreikslpv.filmfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenuButtons()
+    }
+
+    private fun initMenuButtons() {
+        button_menu.setOnClickListener {
+            Toast.makeText(this, button_menu.text, Toast.LENGTH_SHORT).show()
+        }
+        button_collections.setOnClickListener {
+            Toast.makeText(this, button_collections.text, Toast.LENGTH_SHORT).show()
+        }
+        button_favourites.setOnClickListener {
+            Toast.makeText(this, button_favourites.text, Toast.LENGTH_SHORT).show()
+        }
+        button_see_later.setOnClickListener {
+            Toast.makeText(this, button_see_later.text, Toast.LENGTH_SHORT).show()
+        }
+        button_settings.setOnClickListener {
+            Toast.makeText(this, button_settings.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
