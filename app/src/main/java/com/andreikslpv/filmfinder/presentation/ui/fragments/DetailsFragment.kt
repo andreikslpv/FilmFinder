c68294bdeb43972b00cef845720b553b6c49f07e
andreikslpv <andrei.kslpv@yandex.ru>
2023-02-04 13:15:10 +0300

    75: Homework_42: добавлено сохранение постера на устройство

--- app/src/main/java/com/andreikslpv/filmfinder/presentation/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/andreikslpv/filmfinder/presentation/ui/fragments/DetailsFragment.kt
@@ -1,12 +1,19 @@
 package com.andreikslpv.filmfinder.presentation.ui.fragments
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.transition.Fade
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.andreikslpv.filmfinder.R
@@ -18,7 +25,10 @@ import com.andreikslpv.filmfinder.presentation.ui.MainActivity
 import com.andreikslpv.filmfinder.presentation.ui.TRANSITION_DURATION
 import com.andreikslpv.filmfinder.presentation.ui.utils.FragmentsType
 import com.andreikslpv.filmfinder.presentation.ui.utils.loadImage
+import com.andreikslpv.filmfinder.presentation.ui.utils.makeToast
 import com.andreikslpv.filmfinder.presentation.vm.DetailsFragmentViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
@@ -27,6 +37,24 @@ class DetailsFragment : Fragment() {
     private var message: String = ""
     private lateinit var type: FragmentsType
     private val viewModel: DetailsFragmentViewModel by viewModels()
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private val singlePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            when {
+                granted -> {
+                    // доступ к хранилищу разрешен, открываем камеру
+                    performAsyncLoadOfPoster()
+                }
+                !shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+                    // доступ к хранилищу запрещен, пользователь поставил галочку Don't ask again.
+                    // сообщаем пользователю, что он может в дальнейшем разрешить доступ
+                    getString(R.string.details_allow_later_in_settings).makeToast(requireContext())
+                }
+                else -> {
+                    // доступ к хранилищу запрещен, пользователь отклонил запрос
+                }
+            }
+        }
 
     init {
         enterTransition = Fade(Fade.IN).apply { duration = TRANSITION_DURATION }
@@ -121,6 +149,19 @@ class DetailsFragment : Fragment() {
             viewModel.changeWatchLaterField()
             setWatchLaterIcon(viewModel.filmLocalStateLiveData.value?.isWatchLater ?: false)
         }
+        binding.detailsFabDownloadPoster.setOnClickListener {
+            // если Андройд 10+ то сразу запускаем скачивание, иначе запрашиваем разрешение
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                performAsyncLoadOfPoster()
+                return@setOnClickListener
+            }
+            if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // доступ к хранилищу запрещен, нужно объяснить зачем нам требуется разрешение
+                singlePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            } else {
+                singlePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
         binding.detailsFabShare.setOnClickListener {
             //Создаем интент
             val intent = Intent()
@@ -138,6 +179,88 @@ class DetailsFragment : Fragment() {
                 )
             )
         }
+    }
+
+    // ------------- сохранение постера
+    private fun performAsyncLoadOfPoster() {
+        //Создаем родительский скоуп с диспатчером Main потока, так как будем взаимодействовать с UI
+        MainScope().launch {
+            //Включаем Прогресс-бар
+            binding.detailsProgressBar.isVisible = true
+            //Создаем через async, так как нам нужен результат от работы, то есть Bitmap
+            val job = scope.async {
+                viewModel.loadWallpaper(viewModel.filmLocalStateLiveData.value!!.posterDetails)
+            }
+            //Сохраняем в галерею, как только файл загрузится
+            saveToGallery(job.await())
+            //Выводим снекбар с кнопкой перейти в галерею
+            Snackbar.make(
+                binding.root,
+                R.string.details_downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.details_open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+
+            //Отключаем Прогресс-бар
+            binding.detailsProgressBar.isVisible = false
+        }
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        //Проверяем версию системы
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Создаем объект для передачи данных
+            val contentValues = ContentValues().apply {
+                //Составляем информацию для файла (имя, тип, дата создания, куда сохранять и т.д.)
+                put(
+                    MediaStore.Images.Media.TITLE,
+                    viewModel.filmLocalStateLiveData.value!!.title.handleSingleQuote()
+                )
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    viewModel.filmLocalStateLiveData.value!!.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.Images.Media.DATE_ADDED,
+                    System.currentTimeMillis() / 1000
+                )
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/FilmsSearchApp")
+            }
+            //Получаем ссылку на объект Content resolver, который помогает передавать информацию из приложения вовне
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            //Открываем канал для записи на диск
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            //Передаем нашу картинку, может сделать компрессию
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            //Закрываем поток
+            outputStream?.close()
+        } else {
+            //То же, но для более старых версий ОС
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                viewModel.filmLocalStateLiveData.value!!.title.handleSingleQuote(),
+                viewModel.filmLocalStateLiveData.value!!.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
     }
 
 }
